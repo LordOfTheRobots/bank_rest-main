0893a638239a65515a63db5bdb4c0afe25c918db
Pavel <3211232133@mail.ru>
2025-10-23 10:46:32 +0300

    сделаны основы для контроллеров

---
+++ src/main/java/com/example/bankcards/controller/SignInControllerV1.java
@@ -0,0 +1,12 @@
+package com.example.bankcards.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/sign-in")
+public class SignInControllerV1 {
+    public void signIn(){
+        
+    }
+}
